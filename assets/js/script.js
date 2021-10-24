d1ebcc2f9bf7aabb2be338e1edeaf7ab0a2cbe68
Rooster <gallolopez1@hotmail.com>
2021-10-24 16:10:58 -0400

    add feature to dates

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,8 +13,29 @@ var createTask = function(taskText, taskDate, taskList) {
     // append span and p element to parent li
     taskLi.append(taskSpan, taskP);
 
+    // check due date
+    auditTask(taskLi);
+
     // append to ul list on the page
     $("#list-" + taskList).append(taskLi);
+};
+
+var auditTask = function(taskEl) {
+    // get date from task element
+    var date = $(taskEl).find("span").text().trim();
+    // ensure it worked
+    console.log(date);
+
+    //convert to moment object at 5:00pm
+    var time = moment(date, "L").set("hour", 17);
+    // remove any old classes from element
+    $(taskEl).removeClass("list-group-item-warning list-group-item-danger");
+    // apply new class if task is/near over due date
+    if (moment().isAfter(time)) {
+        $(taskEl).addClass("list-group-item-danger");
+    } else if (Math.abs(moment().diff(time, "days")) <= 2) {
+        $(taskEl).addClass("list-group-item-warning");
+    }
 };
 
 var loadTasks = function() {
@@ -114,6 +135,10 @@ $("#trash").droppable({
     }
 });
 
+$("#modalDueDate").datepicker({
+    minDate: 1
+});
+
 
 // modal was triggered
 $("#task-form-modal").on("show.bs.modal", function() {
@@ -205,6 +230,15 @@ $(".list-group").on("click", "span", function() {
         .val(date);
     $(this).replaceWith(dateInput);
 
+    // enable jQuery UI datepicker
+    dateInput.datepicker({
+        minDate: 1,
+        onClose: function() {
+            // when calendar is closed, force a "change" event on the 'dateInput'
+            $(this).trigger("change");
+        }
+    });
+
     // automatically bring up the calendar
     dateInput.trigger("focus");
 });
@@ -241,6 +275,22 @@ $("#remove-tasks").on("click", function() {
     }
     console.log(tasks);
     saveTasks();
+});
+
+$(".list-group").on("change", "input[type='text']", function() {
+    var date = $(this).val();
+
+    var status = $(this).closest(".list-group").attr("id").replace("list-", "");
+    var index = $(this).closest(".list-group-item").index();
+
+    tasks[status][index].date = date;
+    saveTasks();
+
+    var taskSpan = $("<span>").addClass("badge badge-primary badge-pill").text(date);
+    $(this).replaceWith(taskSpan);
+
+    // Pass task's <li> element into auditTask() to check new due date
+    auditTask($(taskSpan).closest(".list-group-item"));
 });
 
 // load tasks for the first time
